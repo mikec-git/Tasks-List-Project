3a7fd79b3686a57372d5dd4a8096ea1e1fe67062
Mike Choi <mike.choi@live.ca>
2018-09-16 18:54:44 -0700

    Added: Add task functionality

---
+++ app.js
@@ -0,0 +1,46 @@
+// Define UI vars
+const form = document.querySelector('#task-form');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.collection');
+const taskInput = document.querySelector('#task');
+const clearBtn = document.querySelector('.clear-tasks');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners(){
+    // Add task event
+    form.addEventListener('submit', addTask);
+}
+
+// Add task
+function addTask(e){
+    e.preventDefault();
+    
+    if(taskInput.value === ''){
+        alert('Add a task');
+    } else{        
+        // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create text node and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        
+        // Create new link element
+        const link = document.createElement('a');
+        // Add class
+        link.className = 'delete-item secondary-content';
+        // Add icon html
+        link.innerHTML = '<i class="fa fa-remove"></i>';
+        // Append link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+
+        // Clear input
+        taskInput.value = '';
+    }
+}
